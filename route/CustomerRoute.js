6188ac958d0a68c5a2e08f59ff1a3f31bf266578
tharanga270 <developerwordpress270@gmail.com>
2023-12-29 19:35:10 +0530

    5.api complete

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const CustomerController = require('../controller/CustomerController');
+const verifyToken = require('../middleware/AuthMiddleware');
 
 const router = express.Router();
 
-router.post('/save-customer', CustomerController.saveCustomer);
-router.put('/update-customer', CustomerController.updateCustomer);
-router.delete('/delete-customer', CustomerController.deleteCustomer);
-router.get('/get-customer', CustomerController.findCustomer);
-router.get('/get-all-customers', CustomerController.findAllCustomer);
+router.post('/save-customer', verifyToken, CustomerController.saveCustomer);
+router.put('/update-customer', verifyToken, CustomerController.updateCustomer);
+router.delete(
+  '/delete-customer',
+  verifyToken,
+  CustomerController.deleteCustomer
+);
+router.get('/get-customer', verifyToken, CustomerController.findCustomer);
+router.get(
+  '/get-all-customers',
+  verifyToken,
+  CustomerController.findAllCustomer
+);
 
 module.exports = router;
